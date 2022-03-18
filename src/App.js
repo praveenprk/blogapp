ad5d2e5e04f86a5d00854f0acde5cd3ec772e75f
praveenprk <praveenkumarkp666@gmail.com>
2022-03-18 22:25:44 +0530

    Cleaned the Code A Bit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-/* import { CssBaseline } from '@mui/material';
-import axios from 'axios';
-import { useEffect, useState } from 'react'; */
-import { Outlet } from 'react-router';
+import { Outlet } from 'react-router'
 import './App.css';
 
 function App() {
 
   return (
     <div className="App">
-      Home
       <Outlet/>
     </div>
   );
 }
 
-export default App;
+export default App
 
