99f2ae6ba456913d8c5700374054038f231519b7
praveenprk <praveenkumarkp666@gmail.com>
2022-03-18 22:24:57 +0530

    Added MUI Skeleton For Blog Cards

--- src/components/BlogPostsCard/BlogPostsSummary.jsx
+++ src/components/BlogPostsCard/BlogPostsSummary.jsx
@@ -1,36 +1,45 @@
-import { Grid } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Link, Outlet } from 'react-router-dom'
+import Skeleton from '@mui/material/Skeleton';
 import './BlogPostsSummary.css'
 
 const BlogPostsSummary = () => {
 
     const [blogPosts, setBlogPosts] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(()=>{
         const fetchPosts = async () => {
         axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(res => {
-            setBlogPosts(res.data)
-        })
+        .then(res => setBlogPosts(res.data))
+        .catch(err => console.log(err))
     }
     fetchPosts()
-}, [])
+},[])
+
+setTimeout(() => {
+    setLoading(false)
+}, 2000)
 
     return  (
         <>
-        <h1>All Blog Posts</h1>
-        <Grid container spacing="2" sx={{
+        <Typography variant='h1'>Blog</Typography>
+        <Grid container spacing="4" sx={{
             textAlign: "center",
             justifyContent: "center",
             padding: '1rem',
             gap: "2rem"
         }}>
             {
-            blogPosts.map(blogPost => {
-            return (
-                    <Grid className="post-card" item key={blogPost.id} md={6} sm={6} xs={12} lg={4}>
+            blogPosts && blogPosts.map(blogPost => {
+                return (
+                    loading ? (
+                    <Skeleton md={6} sm={6} xs={12} lg={3} key={blogPost.id} variant="rectangular" width={300}
+                    height={400}/>
+                    )
+                    : (<Grid className="post-card" item key={blogPost.id} md={6} sm={6} xs={12} lg={4}>
                         <h3>
                             <Link to={`/posts/${blogPost.id}`}>
                                 {blogPost.title.toUpperCase()}
@@ -40,7 +49,7 @@ const BlogPostsSummary = () => {
                             {blogPost.body}
                         </p>
                         <Link to={`/blog/${blogPost.id}`}><span>Read More...</span></Link>
-                    </Grid>
+                    </Grid>)
                 )
         })}
         
