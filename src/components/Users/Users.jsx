43fac1f3b38c3ef7034b2d840447771cb87b0c39
praveenprk <praveenkumarkp666@gmail.com>
2022-03-19 01:08:53 +0530

    Users Component to view all users

---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,93 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import EmailIcon from '@mui/icons-material/Email';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import Avatar from '@mui/material/Avatar';
+import LanguageIcon from '@mui/icons-material/Language';
+import {Grid} from '@mui/material/'
+import { pink, blue, red } from '@mui/material/colors'
+import './Users.css'
+
+const Users = () => {
+  
+const [users, setUsers] = useState([])
+
+
+useEffect(() => {
+  const fetchUsers = async () => {
+    await axios.get(`https://jsonplaceholder.typicode.com/users`)
+    .then(res => setUsers(res.data))
+    .catch(err => console.log(err))
+  }
+
+  fetchUsers()
+
+/* return () => {
+    second
+  } */
+}, [])
+
+console.log(users)
+  
+return (
+    <>
+    <Typography variant='h1'>Users</Typography>
+    <Grid container sx={{
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "1em",
+        padding: '2em',
+    }}>
+    {
+        users.map((user, i) => {
+            return(
+                <Grid item xs={12} sm={6} lg={3} md={4} key={i} sx={{
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: '#f2f2f2',
+                    border: "1px solid transparent",
+                    borderRadius: "10px",
+                    height: "200px",
+                    filter: "drop-shadow(2px 4px 6px black)"
+                }}>
+                    <div className='user-card'>
+                    <div className='user-basic'>
+                        <Avatar
+                        className='avatar'
+                        alt="Remy Sharp"
+                        src="https://mui.com/static/images/avatar/1.jpg"
+                        sx={{ width: 56, height: 56 }}
+                        />
+                        <Typography variant='h6'>{user.name}</Typography>
+                    </div>
+                    <div className='user-cta'>
+                    <IconButton aria-label="email">
+                        <a href={`mailto:${user.email}`}>
+                            <EmailIcon sx={{ color: pink[500] }}/>
+                        </a> 
+                    </IconButton>
+                    <IconButton aria-label="phone">
+                        <a href={`tel:${user.phone}`}>
+                            <LocalPhoneIcon sx={{ color: red[300] }}/>
+                        </a>
+                    </IconButton>
+                    <IconButton aria-label="website">
+                    <a href={`https://${user.website}`}>
+                        <LanguageIcon sx={{ color: blue[500] }}/>
+                    </a>
+                    </IconButton>
+                    </div>
+                    </div>
+                </Grid>
+            )
+        })
+    }
+    </Grid>
+    </>
+)
+
+}
+
+export default Users
