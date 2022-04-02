ddbf4582db3835529c976738aeaf8fb60fa04088
praveenprk <praveenkumarkp666@gmail.com>
2022-04-02 23:14:10 +0530

    Providing Store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import './index.css'
 import Routers from './routers/Routers'
-
+import { store } from './store/store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routers/>
+    <Provider store={store}>
+      <Routers/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
